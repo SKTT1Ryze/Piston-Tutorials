d3386da273d850a8810e2af265c8fa55bf2d52e7
hustccc <1276675421@qq.com>
2020-09-20 19:34:37 +0900

    add some implement

--- getting-started/src/config.rs
+++ getting-started/src/config.rs
@@ -5,3 +5,8 @@ pub const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 pub const YELLOW: [f32; 4] = [1.0, 1.0, 0.0, 0.0];
 pub const PURPLE: [f32; 4] = [1.0, 0.0, 1.0, 0.0];
 pub const BLUE: [f32; 4] = [0.0, 1.0, 1.0, 0.0];
+
+pub const COLOR_NUM: usize = 5;
+pub const COLOR_LIST: [[f32; 4]; COLOR_NUM] =[GREEN, RED, YELLOW, PURPLE, BLUE];
+
+pub const MAX_TICK: usize = 100;
--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -17,35 +17,61 @@ mod config;
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
     rotation: f64,  // Rotation for the square.
+    back_color: [f32; 4],   // Color for the background
+    cube_color: [f32; 4],   // Color for the cube
+    current_color: usize,  // Current color index
+    timer: usize,
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
-        use config::*;
 
         let square = rectangle::square(0.0, 0.0, 50.0);
         let rotation = self.rotation;
         let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+        let backcolor = self.back_color;
+        let cubecolor = self.cube_color;
 
         self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(BLUE, gl);
-
+            
             let transform = c
                 .transform
                 .trans(x, y)
                 .rot_rad(rotation)
                 .trans(-25.0, -25.0);
+            
+            // Clear the screen.
+            clear(backcolor, gl);
 
             // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+            rectangle(cubecolor, square, transform, gl);
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
         self.rotation += 2.0 * args.dt;
+        if self.timer >= config::MAX_TICK {
+            match self.current_color {
+                0 => {
+                    self.cube_color = config::COLOR_LIST[config::COLOR_NUM - 1];
+                },
+                _ => {
+                    self.cube_color = config::COLOR_LIST[self.current_color - 1];
+                }
+            }
+            if self.current_color < config::COLOR_NUM - 1 {
+                self.back_color = config::COLOR_LIST[self.current_color + 1];
+                self.current_color += 1;
+            }
+            else {
+                self.back_color = config::COLOR_LIST[0];
+                self.current_color = 0;
+            }
+            self.timer = 0;
+        }
+        self.timer += 1;
     }
 }
 
@@ -64,6 +90,10 @@ fn main() {
     let mut app = App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0,
+        back_color: config::BLUE,
+        cube_color: config::RED,
+        current_color: 4,
+        timer: 0,
     };
 
     let mut events = Events::new(EventSettings::new());
