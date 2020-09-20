8e59b6b51c7b02c7298f40cb4b4571ee62713584
hustccc <1276675421@qq.com>
2020-09-21 00:29:40 +0900

    small change

--- getting-started/src/config.rs
+++ getting-started/src/config.rs
@@ -5,8 +5,9 @@ pub const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 pub const YELLOW: [f32; 4] = [1.0, 1.0, 0.0, 0.0];
 pub const PURPLE: [f32; 4] = [1.0, 0.0, 1.0, 0.0];
 pub const BLUE: [f32; 4] = [0.0, 1.0, 1.0, 0.0];
+pub const WHITE: [f32; 4] = [1.0; 4];
 
-pub const COLOR_NUM: usize = 5;
-pub const COLOR_LIST: [[f32; 4]; COLOR_NUM] =[GREEN, RED, YELLOW, PURPLE, BLUE];
+pub const COLOR_NUM: usize = 6;
+pub const COLOR_LIST: [[f32; 4]; COLOR_NUM] =[GREEN, RED, YELLOW, PURPLE, BLUE, WHITE];
 
 pub const MAX_TICK: usize = 100;
